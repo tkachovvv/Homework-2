40a6bcf3791edf2503057af1dd75248a14a2d8c6
tkachovvv <dmitiy.tk.skyeng@outlook.com>
2022-10-19 20:09:50 +0300

    Completed the remaining tasks

--- src/Main.java
+++ src/Main.java
@@ -27,5 +27,19 @@ public class Main {
         System.out.println(frog);
         frog = (frog * 10) / 3.5 + 4;
         System.out.println(frog);
+        var firstBoxerWeight = 78.2;
+        var secondBoxerWeight = 82.7;
+        var totalWeight = firstBoxerWeight + secondBoxerWeight;
+        System.out.println(totalWeight);
+        var differenceInWeight = secondBoxerWeight - firstBoxerWeight;
+        System.out.println("Разница в весе бойцов составляет: " + differenceInWeight + "кг ");
+        var differenceInWeight2 = secondBoxerWeight % firstBoxerWeight;
+        System.out.println("Разница в весе бойцов составляет: " + differenceInWeight2 + "кг ");
+        var theAmountOfTime = 640;
+        var timeAtWork = 8.0;
+        var theNumberOfEmployees = theAmountOfTime / timeAtWork;
+        System.out.println("Всего работников в компании: " + theNumberOfEmployees + " человек ");
+        var theUpdatedTimeAtWork = theAmountOfTime / (theNumberOfEmployees + 94);
+        System.out.println("Если в компании работают " + (theNumberOfEmployees + 94) + " человека, " + "то всего " + theUpdatedTimeAtWork + " часов работы может быть поделено между сотрудниками");
     }
 }
