19f1a0617a73cf0e82112a7602f6c51e058ec6cd
tkachovvv <dmitiy.tk.skyeng@outlook.com>
2022-10-19 20:07:54 +0300

    Initiated the friend and the frog variables, multiplied and divided them.

--- src/Main.java
+++ src/Main.java
@@ -19,5 +19,13 @@ public class Main {
         System.out.println(cat);
         paper = paper - 7639;
         System.out.println(paper);
+        var friend = 19;
+        System.out.println(friend);
+        friend = (friend + 2)/7;
+        System.out.println(friend);
+        var frog = 3.5;
+        System.out.println(frog);
+        frog = (frog * 10) / 3.5 + 4;
+        System.out.println(frog);
     }
 }
